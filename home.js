5d2885901b1b81762354e326c4a37f6ad22f79bb
GUSTAVO ZUMBA DE AMORIM <24124573@cfp127aluno.local>
2025-03-13 12:23:51 -0300

    section 1 pronto

---
+++ home.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const images = [
+  {
+    "id": 1,
+    "src": "./img/image2.png",
+    "alt": "Imagem 1"
+  },
+  {
+    "id": 2,
+    "src": "./img/image3.png",
+    "alt": "Imagem 2"
+  },
+  {
+    "id": 3,
+    "src": "./img/image4.png",
+    "alt": "Imagem 3"
+  },
+  {
+    "id": 4,
+    "src": "./img/image5.png",
+    "alt": "Imagem 4"
+  }
+]
+
+let currentImageIndex = 0;
+
+function changeImage() {
+  const imgElement = document.getElementById('central-image')
+  if (imgElement) {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    imgElement.src = images[currentImageIndex].src;
+    imgElement.alt = images[currentImageIndex].alt;
+  }
+}
+
+setInterval(changeImage, 3000);
